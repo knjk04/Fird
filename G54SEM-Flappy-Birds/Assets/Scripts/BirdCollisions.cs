076962b8eee1ba1fac9d8534153cedf5b55a6109
Alessio <alessio3197@gmail.com>
2020-02-13 15:05:16 +0000

    Bird Collisions
    Bird now has collisions installed and when it hits the ceiling or base, the scene resets. BirdCollision script created.

---
+++ G54SEM-Flappy-Birds/Assets/Scripts/BirdCollisions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdCollisions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Currently reloads scene, later update to move to Game Over screen
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Game Over");
+    }
+    
+}
