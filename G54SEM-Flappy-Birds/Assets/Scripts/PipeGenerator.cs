294a230a59b64ba6457e5c8a40aabb3a34476605
Alessio <alessio3197@gmail.com>
2020-02-14 16:25:36 +0000

    Procedurally Generated Pipes
    Pipe generation and destruction now automated, bird-pipe collision tested

---
+++ G54SEM-Flappy-Birds/Assets/Scripts/PipeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGenerator : MonoBehaviour {
+
+    public GameObject pipeSet;
+    public Vector2 spawnValues;
+    public float spawnWait = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPipe());
+    }
+    IEnumerator SpawnPipe()
+    {
+        while (true)
+        {
+            Vector2 spawnPosition = new Vector2(2.0f, 0.165f);
+            Quaternion rotation = Quaternion.identity;
+            GameObject pipe = Instantiate(pipeSet, spawnPosition, rotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
