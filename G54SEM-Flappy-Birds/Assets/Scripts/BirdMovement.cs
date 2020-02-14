84c38d241d651181b483054a7899f02c2a9beba3
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-02-14 19:15:13 +0000

    tilt bird

--- G54SEM-Flappy-Birds/Assets/Scripts/BirdMovement.cs
+++ G54SEM-Flappy-Birds/Assets/Scripts/BirdMovement.cs
@@ -7,17 +7,57 @@ public class BirdMovement : MonoBehaviour
 	// ensure these are set in the inspector
 	public Rigidbody2D RigidBody2D;
 	public float Speed;
+    public float BirdVerticalVelocity;
+
+    public bool BirdTiltedUpwards;
 
 	public void Start() 
 	{
-
+        BirdTiltedUpwards = false;
 	}
 
-	private void FixedUpdate()
+    private void FixedUpdate()
 	{
         if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire1"))
         {
-            RigidBody2D.velocity += new Vector2(0f, 0.75f);
+            RigidBody2D.velocity += new Vector2(0f, BirdVerticalVelocity);
+
+            if (!BirdTiltedUpwards)
+            {
+                Debug.Log("Tilt because birdTiltedUpwards = " + BirdTiltedUpwards);
+                transform.eulerAngles = Vector3.forward * 25;
+                BirdTiltedUpwards = true;
+            }
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.forward * -85;
+            BirdTiltedUpwards = false;
         }
 	}
+
+    void Update()
+    {
+        /*
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire1"))
+        {
+            RigidBody2D.velocity += new Vector2(0f, BirdVerticalVelocity);
+
+            if (!BirdTiltedUpwards)
+            {
+                Debug.Log("Tilt because birdTiltedUpwards = " + BirdTiltedUpwards);
+                transform.eulerAngles = Vector3.forward * 25;
+                BirdTiltedUpwards = true;
+            }
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.forward * -85;
+            BirdTiltedUpwards = false;
+        }
+        */
+        
+
+
+    }
 }
