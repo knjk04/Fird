2484e78cfc3a6bbaa8b31e64a2e34786c8cdb295
Ke Xu <psxkx1@nottingham.ac.uk>
2020-02-18 18:26:39 +0000

    add Score and ready panel

---
+++ G54SEM-Flappy-Birds/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;             // single instance
+
+    public GameObject gameover;                 // game over UI
+    public TextMeshProUGUI Score;                          // score text
+
+    private bool gameOver = false;                  // mark current game status
+    private int score = 0;                          // store score.
+
+    void Start()
+    {
+        Score = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {                     // set single instance
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {         // if gameover and click the picture, restart the game.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void AddScore()                          // when the bird cross a obstacle, add score.
+    {
+        if (gameOver)
+            return;
+
+        score += 1;
+        Score.text = "Score: " + score.ToString();
+    }
+
+    public void GameOver()                          // game over function.
+    {
+        gameover.SetActive(true);
+        gameOver = true;
+    }
+}
