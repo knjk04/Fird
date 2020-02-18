091aec9007b8291ccbdb319bb04e66ff74fbdcaf
Alessio <alessio3197@gmail.com>
2020-02-18 13:07:24 +0000

    Different Height Pipes
    Set of different height pipe sprites created and randomly selected to be procedurally generated in-game

--- G54SEM-Flappy-Birds/Assets/Scripts/PipeGenerator.cs
+++ G54SEM-Flappy-Birds/Assets/Scripts/PipeGenerator.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class PipeGenerator : MonoBehaviour {
 
-    public GameObject pipeSet;
+    public GameObject[] pipeSet;
     public Vector2 spawnValues;
     public float spawnWait = 2;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         StartCoroutine(SpawnPipe());
     }
     IEnumerator SpawnPipe()
     {
         while (true)
         {
+            int pipeSpriteChoice = Random.Range(0, pipeSet.Length);
             Vector2 spawnPosition = new Vector2(2.0f, 0.165f);
             Quaternion rotation = Quaternion.identity;
-            GameObject pipe = Instantiate(pipeSet, spawnPosition, rotation);
+            GameObject pipe = Instantiate(pipeSet[pipeSpriteChoice], spawnPosition, rotation);
             yield return new WaitForSeconds(spawnWait);
         }
     }
