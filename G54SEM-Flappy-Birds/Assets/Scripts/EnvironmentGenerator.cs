5081c511ce47eed55f8eff5aed2a9699f61e9703
Alessio <alessio3197@gmail.com>
2020-02-21 13:52:04 +0000

    Randomise Environment
    Background is now randomised between day and night on each generation of game state. Prefabs created for each situation and script to control random initial generation

---
+++ G54SEM-Flappy-Birds/Assets/Scripts/EnvironmentGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentGenerator : MonoBehaviour {
+    public GameObject[] envs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int pipeSpriteChoice = Random.Range(0, envs.Length);
+        Vector2 spawnPosition = new Vector2(0.0f, 0.0f);
+        Quaternion rotation = Quaternion.identity;
+        GameObject env = Instantiate(envs[pipeSpriteChoice], spawnPosition, rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
