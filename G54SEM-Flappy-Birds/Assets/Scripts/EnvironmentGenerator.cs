335b738a6897826c58b20032904bdcb062c8efec
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-02-25 09:13:30 +0000

    add error checking for environment

--- G54SEM-Flappy-Birds/Assets/Scripts/EnvironmentGenerator.cs
+++ G54SEM-Flappy-Birds/Assets/Scripts/EnvironmentGenerator.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class EnvironmentGenerator : MonoBehaviour {
 
+    // Ensure this is initialised in the inspector:
     public GameObject[] Envs;
 
     // Start is called before the first frame update
     void Start()
     {
-        int PipeSpriteChoice = Random.Range(0, Envs.Length);
-        Vector2 SpawnPosition = new Vector2(0.0f, 0.0f);
-        Quaternion Rotation = Quaternion.identity;
-        GameObject Env = Instantiate(Envs[PipeSpriteChoice], SpawnPosition, Rotation);
+        if (Envs.Length < 1)
+        {
+            Debug.Log("The environments array is empty. Please initialise it.");
+        }
+        else
+        {
+            int PipeSpriteChoice = Random.Range(0, Envs.Length);
+            Vector2 SpawnPosition = new Vector2(0.0f, 0.0f);
+            Quaternion Rotation = Quaternion.identity;
+            GameObject Env = Instantiate(Envs[PipeSpriteChoice], SpawnPosition, Rotation);
+        }
     }
 
     // Update is called once per frame
