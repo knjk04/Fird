93740641e5f3d441cc04ebb7fc517d40e4fe20e8
Alessio <alessio3197@gmail.com>
2020-02-27 14:53:37 +0000

    Bird Flap Animations Added

--- G54SEM-Flappy-Birds/Assets/Scripts/BirdGenerator.cs
+++ G54SEM-Flappy-Birds/Assets/Scripts/BirdGenerator.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class BirdGenerator : MonoBehaviour
 {
 
     public SpriteRenderer spriteRenderer;
+    public Animator animator;
     public Sprite blueBird;
     public Sprite redBird;
     public Sprite yellowBird;
+    public AnimatorController blueAnim;
+    public AnimatorController redAnim;
+    public AnimatorController yellowAnim;
 
-	// Use this for initialization
-	void Start ()
+
+
+    // Use this for initialization
+    void Start ()
     {
         UpdateBirdSprite();
 	}
@@ -26,12 +33,15 @@ public class BirdGenerator : MonoBehaviour
         {
             case 0:
                 spriteRenderer.sprite = blueBird;
+                animator.runtimeAnimatorController = blueAnim;
                 break;
             case 1:
                 spriteRenderer.sprite = redBird;
+                animator.runtimeAnimatorController = redAnim;
                 break;
             case 2:
                 spriteRenderer.sprite = yellowBird;
+                animator.runtimeAnimatorController = yellowAnim;
                 break;
         }
     }
