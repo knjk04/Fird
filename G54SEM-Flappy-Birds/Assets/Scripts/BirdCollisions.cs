fe6a278c2705caaa4e14456e9eaf595622731bea
Xiuyun Peng <alyxp4@nottingham.ac.uk>
2020-03-02 16:44:26 +0000

    Add sound effect on collision

--- G54SEM-Flappy-Birds/Assets/Scripts/BirdCollisions.cs
+++ G54SEM-Flappy-Birds/Assets/Scripts/BirdCollisions.cs
@@ -5,14 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class BirdCollisions : MonoBehaviour 
 {
-    
+    public AudioSource collisonSoundEffect;
+    // only want to play the collision sound effect once
+    private bool collisionSoundEffectPlayed = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //TODO: later update to move to a Game Over panel
         if (collision.gameObject.tag != "score-box")
         {
             Debug.Log("Collision so end the game");
+            if (!collisionSoundEffectPlayed)
+            {
+                collisonSoundEffect.Play();
+                collisionSoundEffectPlayed = true;
+            }
             GameManager.gameInstance.EndGame();
         }
+    }
+
+    public void ResetCollisionSoundEffectPlayed()
+    {
+        collisionSoundEffectPlayed = false;
     }
 }
