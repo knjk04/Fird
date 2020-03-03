86647419ec59dc43c7ea4581b4c27035aee507f4
Tom <allentom97@gmail.com>
2020-03-03 12:45:09 +0000

    finshed pipe destroy tests

--- G54SEM-Flappy-Birds/Assets/Scripts/GameManager.cs
+++ G54SEM-Flappy-Birds/Assets/Scripts/GameManager.cs
@@ -202,7 +202,6 @@ public class GameManager : MonoBehaviour
 
     public bool IsGameOver()
     {
-        Debug.Log("GameOver = " + gameOver);
         return gameOver;
     }
 
--- G54SEM-Flappy-Birds/Assets/Tests/DestroyPipeByTimeTest.cs
+++ G54SEM-Flappy-Birds/Assets/Tests/DestroyPipeByTimeTest.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using UnityEngine.TestTools;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UnityEngine;
 
 public class DestroyPipeByTimeTest
@@ -10,14 +8,6 @@ public class DestroyPipeByTimeTest
     public void DestroyPipeByTimeTestSimplePasses()
     {
         // Use the Assert class to test conditions.
-    }
-
-    // A UnityTest behaves like a coroutine in PlayMode
-    // and allows you to yield null to skip a frame in EditMode
-    [UnityTest]
-    public IEnumerator DestroyPipeByTimeTestWithEnumeratorPasses()
-    {
-        return null;
     }
 
     // Pipes should be destroyed immediately on game over
